dc032c0918b52d2ce77565efe9cdff3fe08a38aa
Kazem Ardekanian <aakazemaa@users.noreply.github.com>
2018-11-15 21:49:14 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(  name='soundmixture',
         version='0.1',
         description='generating mixture of noise and non-noise sound files',
-        url='http://github.com/aakazemaa',
+        url='https://github.com/aakazemaa',
         author='Kazem Ardekanian',
         author_email='kazem.ardekanian@gmail.com',
         license='MIT',
